b6755e69860c573d1db83069b77dba39fab9ba4b
Saad Berrada <72012880+Codrux2200@users.noreply.github.com>
2023-12-18 20:55:48 +0100

    [ADD] github redirection functionel

--- area-react/src/loginPages/GithubPages/github.js
+++ area-react/src/loginPages/GithubPages/github.js
@@ -1,6 +1,6 @@
 
 import { useEffect } from "react";
-
+import axios from "axios";
 
 export const Github = () => {
 
@@ -9,6 +9,22 @@ export const Github = () => {
         console.log(currentUrl);
         const code = currentUrl.split("=")[1];
         console.log(code);
+        const userData = {code};
+        axios
+      .post('http://localhost:8080/auth/github/save', userData, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization' : 'Bearer ' + localStorage.getItem('token'),
+        },
+      }).then((response) => {
+        if (response.status === 200) {
+            console.log("Sucess registration");
+        } else {
+            console.log("Error: " + response.status);
+        }
+      }).catch((error) => {
+        console.log("Error: " + error);
+      });
     });
 
     return(
