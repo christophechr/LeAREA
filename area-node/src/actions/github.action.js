21b514655c865eaeb5d56f198b9230aad1d35e09
Raphaël Mercié <raphael.mercie@epitech.eu>
2023-12-19 17:11:45 +0100

    [ADD] github and weather actions and triggers WORKKKSSS

--- area-node/src/actions/github.action.js
+++ area-node/src/actions/github.action.js
@@ -18,27 +18,31 @@ const getGithubClient = (user, params) => {
 const newUserRepo = async (user, params) => {
     if (!user || !user.githubToken || !params || !params.name) return;
 
-    const { data } = await axios({
-        url: "https://api.github.com/user/repos",
-        method: "post",
-        headers: {
-            Accept: "application/vnd.github+json",
-            Authorization: `Bearer ${user.githubToken}`,
-            "X-GitHub-Api-Version": "2022-11-28",
-        },
-        data: {
-            name: params.name,
-            description: params.description,
-            homepage: params.homepage,
-            private: params.private,
-            has_issues: params.has_issues,
-            has_projects: params.has_projects,
-            has_wiki: params.has_wiki,
-            has_discussions: params.has_discussions,
-        },
-    });
+    try {
+        const { data } = await axios({
+            url: "https://api.github.com/user/repos",
+            method: "post",
+            headers: {
+                Accept: "application/vnd.github+json",
+                Authorization: `Bearer ${user.githubToken}`,
+                "X-GitHub-Api-Version": "2022-11-28",
+            },
+            data: {
+                name: params.name,
+                description: params.description,
+                homepage: params.homepage,
+                private: params.private,
+                has_issues: params.has_issues,
+                has_projects: params.has_projects,
+                has_wiki: params.has_wiki,
+                has_discussions: params.has_discussions,
+            },
+        });
+    } catch (err) {
+        console.log("Cannot create new repository");
+    }
 
-    console.log(data);
+    // console.log(data);
 };
 
 module.exports = {
--- area-node/src/tasks/flows.task.js
+++ area-node/src/tasks/flows.task.js
@@ -8,28 +8,30 @@ const flowTask = async () => {
         flows.forEach((flow) => {
             const [service, action] = flow.trigger.id.split(".");
 
-            User.findById(flow.user).then(async (user) => {
-                try {
-                    const isReady = await triggersConfig[service].triggers[
-                        action
-                    ].function(user, flow.trigger.params);
+            User.findById(flow.user)
+                .populate("githubToken")
+                .then(async (user) => {
+                    try {
+                        const isReady = await triggersConfig[service].triggers[
+                            action
+                        ].function(user, flow.trigger.params);
 
-                    if (isReady) {
-                        const [service, action] = flow.action.id.split(".");
+                        if (isReady) {
+                            const [service, action] = flow.action.id.split(".");
 
-                        try {
-                            actionsConfig[service].actions[action].function(
-                                user,
-                                flow.action.params
-                            );
-                        } catch (err) {
-                            console.log(err);
+                            try {
+                                actionsConfig[service].actions[action].function(
+                                    user,
+                                    flow.action.params
+                                );
+                            } catch (err) {
+                                console.log(err);
+                            }
                         }
+                    } catch (err) {
+                        console.log(err);
                     }
-                } catch (err) {
-                    console.log(err);
-                }
-            });
+                });
         });
     });
 };
