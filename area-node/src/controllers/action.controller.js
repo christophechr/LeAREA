5cc734c3a4d4da1f1f4953ff068caa070fe5a055
Raphaël Mercié <raphael.mercie@epitech.eu>
2023-12-19 17:18:01 +0100

    [FIX] triggers and actions getters

--- area-node/src/controllers/action.controller.js
+++ area-node/src/controllers/action.controller.js
@@ -1,13 +1,25 @@
 const actionsConfig = require("../config/actions.config");
 
 const getActions = (req, res) => {
-    const actions = actionsConfig.map((service) => {
-        service.actions = service.actions.map((action) => {
-            delete action.function;
-            return action;
-        });
-    });
-    res.send(actions);
+    // Remove functions from the actions config object
+    const actions = {};
+
+    for (const [key, value] of Object.entries(actionsConfig)) {
+        actions[key] = {
+            name: value.name,
+            actions: {},
+        };
+
+        for (const [k, v] of Object.entries(actionsConfig[key].actions)) {
+            actions[key].actions[k] = {
+                name: v.name,
+                description: v.description,
+                params: v.params,
+            };
+        }
+    }
+
+    return res.status(200).send(actions);
 };
 
 module.exports = {
--- area-node/src/controllers/trigger.controller.js
+++ area-node/src/controllers/trigger.controller.js
@@ -1,14 +1,25 @@
 const triggersConfig = require("../config/triggers.config");
 
 const getTriggers = (req, res) => {
-    // Remove function from triggers config
-    const triggers = triggersConfig.map((service) => {
-        service.triggers = service.triggers.map((trigger) => {
-            delete trigger.function;
-            return trigger;
-        });
-    });
-    res.send(triggers);
+    // Remove functions from the actions config object
+    const triggers = {};
+
+    for (const [key, value] of Object.entries(triggersConfig)) {
+        triggers[key] = {
+            name: value.name,
+            triggers: {},
+        };
+
+        for (const [k, v] of Object.entries(triggersConfig[key].triggers)) {
+            triggers[key].triggers[k] = {
+                name: v.name,
+                description: v.description,
+                params: v.params,
+            };
+        }
+    }
+
+    return res.status(200).send(triggers);
 };
 
 module.exports = {
