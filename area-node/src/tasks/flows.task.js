2d335ea3e5e01c7c8df764b9baee96e6092f5829
Raphaël Mercié <raphael.mercie@epitech.eu>
2023-12-20 02:54:25 +0100

    [FIX] variable naming

--- area-node/src/tasks/flows.task.js
+++ area-node/src/tasks/flows.task.js
@@ -6,22 +6,25 @@ const triggersConfig = require("../config/triggers.config.js");
 const flowTask = async () => {
     Flow.find({}).then((flows) => {
         flows.forEach((flow) => {
-            const [service, action] = flow.trigger.id.split(".");
+            const [tServiceId, triggerId] = flow.trigger.id.split(".");
 
             User.findById(flow.user).then(async (user) => {
                 try {
                     const isReady = await triggersConfig
-                        .find((trigger) => trigger.id === service)
-                        .triggers.find((trigger) => trigger.id === action)
+                        .find((service) => service.id === tServiceId)
+                        .triggers.find((trigger) => trigger.id === triggerId)
                         .function(user, flow.trigger.params);
 
                     if (isReady) {
-                        const [service, action] = flow.action.id.split(".");
+                        const [aServiceId, actionId] =
+                            flow.action.id.split(".");
 
                         try {
                             actionsConfig
-                                .find((s) => s.id === service)
-                                .actions.find((a) => a.id === action)
+                                .find((service) => service.id === aServiceId)
+                                .actions.find(
+                                    (action) => action.id === actionId
+                                )
                                 .function(user, flow.action.params);
                             console.log("Flow executed");
                         } catch (err) {
