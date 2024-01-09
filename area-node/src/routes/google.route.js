7e6981ca1bc82a8cdf6c542d21a97fbb4c043edd
Raphaël Mercié <raphael.mercie@epitech.eu>
2024-01-09 15:21:13 +0100

    [ADD] Google OAuth //WIP

---
+++ area-node/src/routes/google.route.js
@@ -0,0 +1,18 @@
+const authControllers = require("../controllers/auth.controller.js");
+const { auth, getUser } = require("../middlewares/auth.middleware.js");
+const googleControllers = require("../controllers/google.controller.js");
+
+// preHandlers are like middleware !
+module.exports = function (fastify, opts, done) {
+    fastify.get(
+        "/google",
+        { preHandler: [auth, getUser] },
+        googleControllers.getOAuthGoogleAddress
+    );
+    fastify.post(
+        "/google",
+        { preHandler: [auth, getUser] },
+        googleControllers.googleOAuthCallback
+    );
+    done();
+};
