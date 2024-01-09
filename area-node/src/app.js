50551d3064c76c2178604c355bc32aeb9eddf3d1
Raphaël Mercié <raphael.mercie@epitech.eu>
2024-01-09 20:23:25 +0100

    [ADD] Gmail callback when email received

--- area-node/src/app.js
+++ area-node/src/app.js
@@ -55,4 +55,35 @@ const start = async () => {
         process.exit(1);
     }
 };
+
+
+const { PubSub } = require('@google-cloud/pubsub');
+
+const projectId = process.env.GOOGLE_PROJECT_ID;
+const subscriptionName = process.env.GOOGLE_SUB_NAME;
+
+const pubsub = new PubSub({ projectId });
+
+const subscription = pubsub.subscription(subscriptionName);
+
+function handleMessage(message) {
+    try {
+        const data = JSON.parse(Buffer.from(message.data, 'base64').toString());
+
+        // Extract relevant information from the message payload
+        console.log('Received message:', data);
+
+    } catch (e) {
+        console.error(e);
+    }
+
+    // Acknowledge the message to remove it from the subscription
+    message.ack();
+}
+
+// Listen for new messages
+subscription.on('message', handleMessage);
+
+
+
 start();
