a4fadae07f1c843cf4e9a0c743ef2cbe4a09fe2e
Raphaël Mercié <raphael.mercie@epitech.eu>
2024-01-12 12:46:35 +0100

    [ADD] datetime API

--- area-node/src/controllers/flows.controller.js
+++ area-node/src/controllers/flows.controller.js
@@ -2,6 +2,11 @@ const Flow = require("../models/flow.model.js");
 const triggerConfig = require("../config/triggers.config.js");
 const actionConfig = require("../config/actions.config.js");
 
+
+const dateTimeRegex = new RegExp(
+    "^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:00Z$"
+);
+
 const getUserFlows = async (req, res) => {
     try {
         const flows = await Flow.find({ user: req.user._id });
@@ -59,6 +64,12 @@ const checkParams = (params, config, res) => {
         }
         if (param.type === "string") {
             if (typeof params[param.id] !== "string")
+                return res.status(400).send({
+                    message: `Invalid parameter value: ${param.id}`,
+                });
+        }
+        if (param.type === "datetime") {
+            if (typeof params[param.id] !== "string" || !dateTimeRegex.test(params[param.id]))
                 return res.status(400).send({
                     message: `Invalid parameter value: ${param.id}`,
                 });
