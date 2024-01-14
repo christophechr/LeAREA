652de9412ad5c82d21d07b24a174e75fae3a5221
Saad Berrada <72012880+Codrux2200@users.noreply.github.com>
2024-01-14 15:06:14 +0100

    [ADD] creation new Workflow completed

--- area-expo/Utils/Utils.js
+++ area-expo/Utils/Utils.js
@@ -1,6 +1,6 @@
 
 import { SelectList } from 'react-native-dropdown-select-list'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import RNPickerSelect from 'react-native-picker-select';
 import { useState, useEffect } from 'react';
 
@@ -10,14 +10,75 @@ export const Input_Selector = ({data, setaction, action}) =>{
     if (data != undefined) {
         newArray = Object.values(data).map((val) => {return {value : val, label : val.name}});
     }
+
     return(
         <View>
              <RNPickerSelect
              textInputProps={{fontSize : 30}}
-            onValueChange={(value) => console.log(value)}
+            onValueChange={setSelected}
             items={newArray}
+            onClose={() => {setaction(selected)}}
         />
         </View>
+    );
+}
+
+
+export const CustomInput_Selector = ({setVal, Val, action}) =>{
+    const [value, setValue] = useState(Val[action.id]);
+    let newArray = [];
+    if (action != undefined) {
+        newArray = action.values.map((val) => {return {value : val.value, label : val.name}});
+    }
+
+    return(
+        <View>
+            <Text style = {{fontWeight : 'bold'}}>{action.name}</Text>
+            <View style = {{borderWidth : 1, borderColor : "black", width : 200}}>
+             <RNPickerSelect
+             textInputProps={{fontSize : 20, color : 'black'}}
+            onValueChange={setValue}
+            items={newArray}
+            onClose={() => {const list = Val;
+                list[action.id] = value;
+                setVal({...list})}}
+        />
+        </View>
+        </View>
+    );
+}
+
+
+
+export const CustomTextInput = ({setVal, Val, action}) => {
+    const [value, setValue] = useState(Val[action.id]);
+
+    return(
+        <View>
+            <Text style = {{fontWeight : 'bold'}}>{action.name}</Text>
+        <TextInput style = {{borderWidth : 1, borderColor : 'black', width : 200}} placeholder={action.description} value = {value} onChangeText={(val) => {setValue(val)}} 
+        onBlur={() => {const list = Val;
+            list[action.id] = value;
+            setVal({...list})}}></TextInput>
+        </View>
+
+    );
+
+}
+
+
+export const CustomNumberInput = ({setVal, Val, action}) => {
+    const [value, setValue] = useState(Val[action.id]);
+
+    return(
+        <View>
+            <Text style = {{fontWeight : 'bold'}}>{action.name}</Text>
+        <TextInput keyboardType = 'number-pad' style = {{borderWidth : 1, borderColor : 'black', width : 200}} placeholder={action.description} value = {value} onChangeText={(val) => {setValue(val)}} 
+        onBlur={() => {const list = Val;
+            list[action.id] = parseInt(value);
+            setVal({...list})}}></TextInput>
+        </View>
+
     );
 
 }
--- area-expo/Workflow/Workflow.js
+++ area-expo/Workflow/Workflow.js
@@ -1,4 +1,4 @@
-import { View, Button, TouchableOpacity, Text, TextInput, Image } from "react-native"
+import { View, Button, TouchableOpacity, Text, TextInput, Image , Alert} from "react-native"
 import { LogoutUser } from "../Login/login"
 import { useNavigation } from "@react-navigation/native"
 import { LinearGradient } from 'expo-linear-gradient';
@@ -6,7 +6,7 @@ import styles from "./css/Workflow.module.css"
 import { useEffect, useState } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import { Input_Selector } from "../Utils/Utils";
+import { CustomInput_Selector, CustomNumberInput, CustomTextInput, Input_Selector } from "../Utils/Utils";
 import {IP} from "@env"
 import RNPickerSelect from 'react-native-picker-select';
 
@@ -52,43 +52,116 @@ export const ChosePage = (props) => {
     );
 }
 
+const createWorkflow = async (actionid, actionparams, triggerid, triggerparams, name, setContinuer) => {
+    const json = {
+        "name" : name,
+        "trigger" : {
+            "id" : triggerid,
+            "params" : triggerparams,
+        },
+        "action" : {
+            "id" : actionid,
+            "params" : actionparams,
+        }
+    }
+    const token = await AsyncStorage.getItem("token");
+    console.log(json);
+    axios
+      .post("http://" + IP + ':8080/flows', json, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization' : 'Bearer ' + token
+        },
+      })
+      .then((response) => {
+        if (response.status === 201) {
+          Alert.alert("created workflow successfully");
+          setContinuer(1);
+        } else {
+          Alert.alert("il y a peux être eu un soucis", response.status);
+          setContinuer(1);
+        }
+      })
+      .catch((error) => {
+        console.error("Error: ", error.message);
+        Alert.alert(error.message);
+      });
+}
+
+
 
 
 
 const Create_Workflow = () => {
     const navigation = useNavigation();
     const [choseAction, setChoseAction] = useState({});
-    const [chosesousaction, setchosesousaction] = useState({});
-    const [chosesoustrigger, setchosesoustrigger] = useState({});
     const [choseTrigger, setChoseTrigger] = useState({});
     const [name, setName] = useState("");
     const [continuer, setContinuer] = useState(1);
-    const [actionparams, setactionparams] = useState({});
-    const [triggerparams, settriggerparams] = useState({});
-
-    const ChooseTrigger = () => {
-        return(
-            <View>
-                <Text style = {{fontSize : 50, alignSelf : 'center'}}>On</Text>
-               <Input_Selector data = {choseAction.actions} setaction={setchosesousaction} ></Input_Selector>
-            <Text style = {{fontSize : 50, alignSelf : 'center'}}>Do</Text>
-            <Input_Selector data = {choseTrigger.triggers} setaction={setchosesoustrigger} ></Input_Selector>
-            </View>
-        );
-    }
+    const [chosesousaction, setchosesousaction] = useState({});
+    const [chosesoustrigger, setchosesoustrigger] = useState({});
+    const url = "http://" + IP + ":8080";
 
     const SetArg = () => {
-        console.log(chosesousaction.params);
+        const [actionparams, setactionparams] = useState({});
+        const [triggerparams, settriggerparams] = useState({});
+        const [change, setchange] = useState(true);
+       console.log(chosesoustrigger);
         return(
             <View>
-
+            {!change ?
+            <View style = {{width : 'auto', height : 'auto', minHeight : 300, backgroundColor : 'rgba(255,255,255, 0.5)', marginBottom : 20, borderRadius : 10}}>
+                <View style = {{padding : 20, flexDirection : 'column', justifyContent : 'space-around', minHeight : 250}}>
+                    <Image source = {{uri : url + choseTrigger.img}} style = {{width : 30, height : 30, alignSelf : 'center'}}></Image>
+                    {
+                        chosesoustrigger.params.map((val) => {
+                            return(
+                                <View>
+                                    {
+                                        val.type == "string" ?
+                                        <CustomTextInput setVal = {settriggerparams} Val = {triggerparams} action = {val}/>
+                                        : val.type == "enum" || val.type == "boolean" ?
+                                        <CustomInput_Selector setVal = {settriggerparams} Val = {triggerparams} action = {val} /> : 
+                                        <CustomNumberInput setVal = {settriggerparams} Val = {triggerparams} action = {val}/>
+                                    }
+                                </View>
+                            );})
+                    }
+                </View>
             </View>
+            : 
+            <View style = {{width : 'auto', height : 'auto', minHeight : 300, backgroundColor : 'rgba(255,255,255, 0.5)', marginBottom : 20, borderRadius : 10}}>
+                <View style = {{padding : 20, flexDirection : 'column', justifyContent : 'space-around', minHeight : 250}}>
+                    <Image source = {{uri : url + choseAction.img}} style = {{width : 30, height : 30, alignSelf : 'center'}}></Image>
+                    {
+                        chosesousaction.params.map((val) => {
+                            return(
+                                <View>
+                                    {
+                                        val.type == "string" ?
+                                        <CustomTextInput setVal = {setactionparams} Val = {actionparams} action = {val}/>
+                                        : val.type == "enum" || val.type == "boolean" ?
+                                        <CustomInput_Selector setVal = {setactionparams} Val = {actionparams} action = {val} /> : <></>
+                                    }
+                                </View>
+                            );})
+                    }
+                </View>
+            </View>}
+                <View style = {{flexDirection : 'row-reverse'}}>
+                    <TouchableOpacity onPress={() => {change ? setchange(false) : createWorkflow(choseAction.id + "." + chosesousaction.id, actionparams, choseTrigger.id + "." + chosesoustrigger.id, triggerparams, name , setContinuer) }} style = {{alignSelf : 'center', backgroundColor : 'blue', borderRadius : 10, width : 150, height : 50, justifyContent : 'center'}}>
+                                <Text style = {{color : 'white', alignSelf : 'center', fontWeight : 'bold'}}>Suivant</Text>
+                                </TouchableOpacity>
+                
+                    <TouchableOpacity onPress={() => {!change ? setchange(true) : setContinuer(continuer - 1)}} style = {{alignSelf : 'center', backgroundColor : 'blue', borderRadius : 10, width : 150, height : 50, justifyContent : 'center'}}>
+                    <Text style = {{color : 'white', alignSelf : 'center', fontWeight : 'bold'}}>Retour</Text>
+                    </TouchableOpacity> 
 
+                </View>
+            </View>
         );
     }
 
-
-    const url = "http://" + IP + ":8080";
     return(
         <View style = {{flexDirection : 'column', justifyContent : 'space-between', height : "40%"}}>
 
@@ -109,21 +182,31 @@ const Create_Workflow = () => {
                 
                 </View> 
                 : continuer == 2 ?
-                <ChooseTrigger></ChooseTrigger> 
-                : continuer == 3 ? 
-                <></> 
+                <View>
+                    <Text style={{ fontSize: 50, alignSelf: 'center' }}>On</Text>
+                    <Input_Selector data={choseAction.actions} setaction={setchosesousaction}></Input_Selector>
+                    <Text style={{ fontSize: 50, alignSelf: 'center' }}>Do</Text>
+                    <Input_Selector data={choseTrigger.triggers} setaction={setchosesoustrigger}></Input_Selector>
+                </View>
+                :continuer == 3 ?
+                <View style = {{flexDirection : 'column'}}>
+                    <Text style = {{fontWeight : 'bold', fontSize : 20, marginBottom : 30}}>Set a Valid Name to your Workflow</Text>
+                    <TextInput style = {{borderWidth : 1, borderColor : 'black', width : 200, alignSelf : 'center', fontSize : 20}} value = {name} onChangeText={setName}/>
+                </View>
+                : continuer == 4 ? 
+                <SetArg/> 
                 :
                 <></>
             }
             <View style = {{flexDirection : 'row-reverse', justifyContent : 'space-around'}}>
                 {
-                    Object.keys(choseAction).length != 0 && Object.keys(choseTrigger).length != 0 ?
-                    <TouchableOpacity onPress={() => {setContinuer(continuer + 1)}} style = {{alignSelf : 'center', backgroundColor : 'blue', borderRadius : 10, width : 150, height : 50, justifyContent : 'center'}}>
+                    Object.keys(choseAction).length != 0 && Object.keys(choseTrigger).length != 0 && continuer < 4?
+                    <TouchableOpacity onPress={() => {continuer == 3 && name == "" ? Alert.alert("to continue you must put a name") : setContinuer(continuer + 1)}} style = {{alignSelf : 'center', backgroundColor : 'blue', borderRadius : 10, width : 150, height : 50, justifyContent : 'center'}}>
                         <Text style = {{color : 'white', alignSelf : 'center', fontWeight : 'bold'}}>Continuer</Text>
                         </TouchableOpacity>:<></>
                 }
                 {
-                    continuer > 1 ? 
+                    continuer > 1 && continuer < 4? 
                     <TouchableOpacity onPress={() => {setContinuer(continuer - 1)}} style = {{alignSelf : 'center', backgroundColor : 'blue', borderRadius : 10, width : 150, height : 50, justifyContent : 'center'}}>
                     <Text style = {{color : 'white', alignSelf : 'center', fontWeight : 'bold'}}>Retour</Text>
                     </TouchableOpacity>:<></>
