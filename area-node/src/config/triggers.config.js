9a754a6b459621d05cab61a39e2b21b1f05b9723
Raphaël Mercié <raphael.mercie@epitech.eu>
2024-01-16 21:22:23 +0100

    [DEL] useless code

--- area-node/src/config/triggers.config.js
+++ area-node/src/config/triggers.config.js
@@ -126,51 +126,6 @@ module.exports = [
         name: "Micropayment",
         img: "/public/images/micropayment.webp",
         triggers: [
-            // Not implemented, can be erased
-            // {
-            //     id: "incommingPayment",
-            //     name: "When an incomming payment is received...",
-            //     description: "Triggers a micropayment condition.",
-            //     function: triggers.micpmtTriggers.incommingPayment,
-            //     params: [
-            //         {
-            //             id: "operator",
-            //             name: "Operator",
-            //             type: "enum",
-            //             required: true,
-            //             description: "The operator to compare the amount.",
-            //             values: [
-            //                 {
-            //                     name: "Equal",
-            //                     value: "equal",
-            //                 },
-            //                 {
-            //                     name: "Greater than",
-            //                     value: "greater_than",
-            //                 },
-            //                 {
-            //                     name: "Less than",
-            //                     value: "less_than",
-            //                 },
-            //                 {
-            //                     name: "Greater than or equal",
-            //                     value: "greater_than_or_equal",
-            //                 },
-            //                 {
-            //                     name: "Less than or equal",
-            //                     value: "less_than_or_equal",
-            //                 },
-            //             ]
-            //         },
-            //         {
-            //             id: "value",
-            //             name: "Value",
-            //             type: "number",
-            //             required: true,
-            //             description: "The value to compare the last payment.",
-            //         },
-            //     ]
-            // },
             {
                 id: "balance",
                 name: "Check the balance...",
